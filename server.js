4addc648f48b6d0bf015a5e2a244afe5fd08927f
VeryGoodOffDev <96524370+veryGoodOffDev@users.noreply.github.com>
2025-12-03 22:58:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5501;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 const API_KEY = process.env.YANDEX_RASP_API_KEY;
 
 // Удельная, код Яндекс.Расписаний
